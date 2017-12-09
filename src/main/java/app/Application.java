58ed0cdd7d4920e6d6c8eaa39292c6fac6d12384
stdan85 <stdan85@gmail.com>
2017-12-09 17:42:23 +0300

    cors

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,9 +2,11 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
+@CrossOrigin
 public class Application {
 
     public static void main(String[] args) {
