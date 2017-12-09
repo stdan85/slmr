3923c873e155df6025f1e80e6737ca0d58e7e5e9
stdan85 <stdan85@gmail.com>
2017-12-09 16:22:04 +0300

    1

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,29 @@
+package app;
+
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Properties;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String str = "name=Ivan;surname=Petrov;age=18";
+
+        Properties prop = new Properties();
+
+        prop.load(new StringReader(str.replaceAll(";","\n")));
+
+        System.out.println(new Gson().toJson(prop));
+
+    }
+
+
+}
+
